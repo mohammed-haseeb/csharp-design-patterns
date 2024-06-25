d38d3773967c4ad6136bc153bd578e5d7038e1be
Mohammed Haseeb <mdhaseeb.98@gmail.com>
2024-06-25 15:00:01 +0530

    Implemented Open-Close Principle

---
+++ csharp-design-patterns/SOLID/OCP.cs
@@ -0,0 +1,170 @@
+﻿using static System.Console;
+
+namespace csharp_design_patterns.SOLID.OCP
+{
+
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color)
+                    yield return p;
+        }
+
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+                if (p.Size == size)
+                    yield return p;
+        }
+
+        public IEnumerable<Product> FilterByColorAndSize(IEnumerable<Product> products, Color color, Size size)
+        {
+            foreach (var p in products)
+                if (p.Color == color && p.Size == size)
+                    yield return p;
+        }
+
+        // so each time I want to add a new filter, I need to comeback and modify the ProductFilter class - Violation of OCP
+    }
+
+    // Instead we create interfaces (so that we can simply extend when needed rather than modifying
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> products, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+
+    // combinator
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> products, ISpecification<Product> spec)
+        {
+            foreach (var i in products)
+                if (spec.IsSatisfied(i))
+                    yield return i;
+        }
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Red, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            WriteLine("--- Old Way ---");
+            WriteLine("Green Products:");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                WriteLine($" - {p.Name} is green");
+            }
+
+            // ^^^ BEFORE ^^^
+
+            // vvv AFTER vvv
+            var bf = new BetterFilter();
+            WriteLine("--- New Way ---");
+            WriteLine($"Greeen Products");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                WriteLine($" - {p.Name} is green");
+            }
+
+            WriteLine("Large products");
+            foreach (var p in bf.Filter(products, new SizeSpecification(Size.Large)))
+                WriteLine($" - {p.Name} is large");
+
+            WriteLine("Large blue items");
+            foreach (var p in bf.Filter(products,
+              new AndSpecification<Product>(new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large)))
+            )
+            {
+                WriteLine($" - {p.Name} is big and blue");
+            }
+
+        }
+    }
+
+}
+
+
+
+/* Notes:
+- we implement ocp by referring to abstractions for dependencies such as interfaces or abstract classes.
+- In this case, I need to be able to extend the ProductFilter class, i.e. make new filters when needed but cannot go and change the existing class(ProdcutFilter) itself(which we do in the first example) since it might already, say be shipped to the customer.
+- Here we use "Specifications Pattern" which basically allows us to check if objects meet certain requirements or in other words, if object matches some criteria.
+*/
